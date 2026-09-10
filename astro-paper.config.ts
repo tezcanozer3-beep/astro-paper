7c09ea86a783b89101e73a7735be183ceb506a9c
tezcanozer3-beep <tezcanozer3@gmail.com>
2026-09-11 01:49:10 +0300

    goo

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -28,17 +28,14 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
+
     { name: "mail",     url: "mailto:hello@primeattest.com" },
+    { name: "whatsapp", url: "https://wa.me/?trxt="}
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x",        url: "https://x.com/intent/post?url=" },
-    { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+
     { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
   ],
 });
