15e746b8c6e1c713bdd16a7b709c9e1d02a9c394
tezcanozer3-beep <tezcanozer3@gmail.com>
2026-09-11 01:44:16 +0300

    go

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["tr", "en"],
+      defaultLocale: "tr",
     routing: {
       prefixDefaultLocale: false,
     },
